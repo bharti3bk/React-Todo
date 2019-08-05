1281b1296a746f43a196a2b103fd1add9384137e
Bharti <bharti@Admins-MacBook-Pro.local>
2019-08-05 13:04:15 -0700

    Added Structure of Components

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,3 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js   
-import React from 'react';
+import React , {Component} from 'react';
+import ToDo from '../TodoComponents/Todo';
+
+class TodoComponent extends Component {
+    constructor(){
+        super();
+    }  
+   render(){
+       return(
+           <div></div>
+       )
+   }
+}  
+
+export default TodoComponent;
